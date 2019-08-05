afde857f431e40204cfcea088b2038128a630f2e
Stefan Büttner <buettner.stefan@web.de>
2019-08-05 21:18:47 +0200

    Update use.sh with cli.

--- use.sh
+++ use.sh
@@ -16,6 +16,44 @@ function backup {
 }
 
 # Setup vim
-backup ${HOME}/.vimrc
-ln -s ${SCRIPT_DIR}/vim/vimrc ${HOME}/.vimrc
+SETUP_VIM=false
+function setup_vim {
+    if [ $SETUP_VIM == true ]; then
+        backup ${HOME}/.vimrc
+        ln -s ${SCRIPT_DIR}/vim/vimrc ${HOME}/.vimrc
+    fi
+}
+
+function print_help {
+    echo "$0 program"
+    echo ""
+    echo "Creates symbolic links for the chosen programs from your home to the configuration files in this repository."
+    echo ""
+    echo "  program - One or more of: vim"
+    echo ""
+}
+
+while (($#))
+do
+	case $1 in
+		--help | -h)
+			print_help ;
+			exit ;
+		;;
+		vim)
+            SETUP_VIM=true
+		;;
+		*)
+			echo "Unknown parameter '$1'. Try --help for more information."
+		exit 1
+	esac
+	shift
+done
+
+if [ !$SETUP_VIM ]; then
+    echo "No program given. See --help for more information"
+    exit 1
+fi
+
+setup_vim
 
