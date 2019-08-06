f74348a68a2decb3fe363f18f2f5c0d5c60b822e
Stefan Büttner <buettner.stefan@web.de>
2019-08-06 14:26:25 +0200

    Add i3 and i3status configs.

--- use.sh
+++ use.sh
@@ -5,23 +5,41 @@ function backup {
     TARGET=$1
     BACKUP=${TARGET}.bak
     if [ -f ${TARGET} ]; then
-        echo "${TARGET} already exists. Moving to ${BACKUP}."
+        echo "Trying to backup ${TARGET}."
         if [ -f ${BACKUP} ]; then
-            echo "${BACKUP} also already exists. Abort."
-            exit 1
+            echo "${BACKUP} also already exists. Skipping."
+            return 1
+        else
+            echo "Moving to ${BACKUP}."
+            mv ${TARGET} ${BACKUP}
+            return 0
         fi
-        mv ${TARGET} ${BACKUP}
     fi
-
+    return 0
 }
 
 # Setup vim
 SETUP_VIM=false
 function setup_vim {
     if [ $SETUP_VIM == true ]; then
-        backup ${HOME}/.vimrc
-        ln -s ${SCRIPT_DIR}/vim/vimrc ${HOME}/.vimrc
+        if backup ${HOME}/.vimrc; then
+            ln -s ${SCRIPT_DIR}/vim/vimrc ${HOME}/.vimrc
+        fi
     fi
+}
+
+# Setup i3
+SETUP_I3=false
+function setup_i3 {
+  if [ $SETUP_I3 == true ]; then
+    if backup ${HOME}/.config/i3/config; then
+        ln -s ${SCRIPT_DIR}/i3/i3-config ${HOME}/.config/i3/config
+    fi
+
+    if backup ${HOME}/.config/i3status/config; then
+        ln -s ${SCRIPT_DIR}/i3/i3status-config ${HOME}/.config/i3status/config
+    fi
+  fi
 }
 
 function print_help {
@@ -29,7 +47,7 @@ function print_help {
     echo ""
     echo "Creates symbolic links for the chosen programs from your home to the configuration files in this repository."
     echo ""
-    echo "  program - One or more of: vim"
+    echo "  program - One or more of: vim, i3"
     echo ""
 }
 
@@ -43,6 +61,9 @@ do
 		vim)
             SETUP_VIM=true
 		;;
+        i3)
+            SETUP_I3=true
+        ;;
 		*)
 			echo "Unknown parameter '$1'. Try --help for more information."
 		exit 1
@@ -50,10 +71,10 @@ do
 	shift
 done
 
-if [ !$SETUP_VIM ]; then
+if [ ! $SETUP_VIM -a ! $SETUP_I3 ]; then
     echo "No program given. See --help for more information"
     exit 1
 fi
 
 setup_vim
-
+setup_i3
